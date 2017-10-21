a399b8e78e2e8f6dc6fad84194c129e5ecafb003
Webmaster444 <jlmobile710@gmail.com>
2017-10-21 10:13:37 -0600

    add csrf token

--- Laravel54/app/Http/Controllers/CRUDController.php
+++ Laravel54/app/Http/Controllers/CRUDController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class CRUDController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('crud.create');
+    }
+    public function store(Request $request)
+    {
+        $crud = new Crud([
+          'title' => $request->get('title'),
+          'post' => $request->get('post')
+        ]);
+
+        $crud->save();
+        return redirect('/crud');
+    }
 }
